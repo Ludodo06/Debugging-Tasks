e84db9de9a1bde94f520b17221497452581b2545
Ludo <pujolludo@gmail.com>
2024-04-19 10:35:45 +0200

    autosave

--- Exercice 0 - Leçon/test.py
+++ Exercice 0 - Leçon/test.py
@@ -4,8 +4,9 @@ max = 0
 @onevent
 def acc():
     global min, max
-    min = acc[2] if acc[2] < min
-    print(acc[2])
+    min = acc[2] if acc[2] < min else min
+    max = acc[2] if acc[2] > max else max
+    print(acc[2], min, max)
     
 #motor_right_target = 200
 #motor_left_target = 200
