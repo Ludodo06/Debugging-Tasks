b01c11614dd0f5182c66d460b6d697967a7bad3c
Ludo <pujolludo@gmail.com>
2024-04-19 10:37:53 +0200

    autosave

--- Exercice 0 - Leçon/test.py
+++ Exercice 0 - Leçon/test.py
@@ -8,8 +8,10 @@ def acc():
     max = acc[2] if acc[2] > max else max
     print(acc[2], min, max)
     
+    
+timer_period[0] = 2000
 @onevent
 def timer0():
-    global        
+    global motor_left_target, motor_right_target        
     motor_right_target = 200
     motor_left_target = 200
