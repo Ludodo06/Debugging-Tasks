bde060d27328b2b98b6600138f6251437d00a652
Ludo <pujolludo@gmail.com>
2024-04-19 10:32:33 +0200

    autosave

--- Exercice 0 - Leçon/test.py
+++ Exercice 0 - Leçon/test.py
@@ -1,6 +1,6 @@
 @onevent
 def acc():
-    print(acc[0], acc[1], acc[2])
+    print(acc[2])
     
-motor_right_target = 400
-motor_left_target = 400
+motor_right_target = 200
+motor_left_target = 200
