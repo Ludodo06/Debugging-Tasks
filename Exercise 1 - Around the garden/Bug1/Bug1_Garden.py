0ef0f3bd7f602852637f853e63f664fa1963aa43
Ludo <pujolludo@gmail.com>
2024-04-21 15:21:27 +0200

    autosave

--- Exercise 1 - Around the garden/Bug1/Bug1_Garden.py
+++ Exercise 1 - Around the garden/Bug1/Bug1_Garden.py
@@ -1,23 +1,23 @@
-forward_time = 4000
-turn_time = 1100
-timer_period[0] = 10000
-going_forward = False
-
-
-@onevent
-def timer0():
-    global leds_top, going_forward, motor_left_target, motor_right_target
-    going_forward = not going_forward
-    if going_forward:
-        timer_period[0] = forward_time
-        nf_leds_top(0,32,0) # Green
-        motor_right_target = 200
-        motor_left_target = 200
-    else:
-        timer_period[0] = turn_time
-        nf_leds_top(32,16,0) # Orange
-        motor_right_target = 200
-        motor_left_target = -200
+# forward_time = 4000
+# turn_time = 1100
+# timer_period[0] = 10000
+# going_forward = False
+# 
+# 
+# @onevent
+# def timer0():
+#     global leds_top, going_forward, motor_left_target, motor_right_target
+#     going_forward = not going_forward
+#     if going_forward:
+#         timer_period[0] = forward_time
+#         nf_leds_top(0,32,0) # Green
+#         motor_right_target = 200
+#         motor_left_target = 200
+#     else:
+#         timer_period[0] = turn_time
+#         nf_leds_top(32,16,0) # Orange
+#         motor_right_target = 200
+#         motor_left_target = -200
         
         
         
@@ -46,7 +46,7 @@ print(" --- Debut du programme --- ")
         
 # To avoid flood, can print once every second if manipulated
 can_print = True
-def timer0():
+def timer1():
     global can_print
     print("OKKKK")
     can_print = True
@@ -70,13 +70,13 @@ def acc():
     elif (acc[0] > 3 or acc[0] < -3 or acc[1] > 4 or acc[1] < -4) and (left_last !=0 or right_last !=0) and can_print:
         print("Thymio manipulated", acc[0], acc[1])
         can_print = False
-        timer_period[0] = 1000
+        timer_period[1] = 1000
         
     # Be less indulgent for the thresholds if Thymio is not supposed to move    
     if (acc[0] > 3 or acc[0] < -3 or acc[1] > 3 or acc[1] < -3)  and (left_last ==0 or right_last ==0) and can_print:
         print("Thymio manipulated", acc[0], acc[1])
         can_print = False
-        timer_period[0] = 1000
+        timer_period[1] = 1000
         
 
 # Button prints
