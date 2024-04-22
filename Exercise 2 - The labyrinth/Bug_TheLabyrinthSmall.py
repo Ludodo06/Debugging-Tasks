771ede7bc11cdb2ca5640425576fbd17a44393dd
Jerome Amiguet <jerome.amiguet@alumni.epfl.ch>
2024-04-23 00:15:51 +0200

    autosave

--- Exercise 2 - The labyrinth/Bug_TheLabyrinthSmall.py
+++ Exercise 2 - The labyrinth/Bug_TheLabyrinthSmall.py
@@ -10,7 +10,7 @@ def timer0():
     if current_step == 6: left()
     if current_step == 7: forward()
     if current_step == 8: right()
-    if current_step == 9: forward()
+    if current_step = 9: forward()
     if current_step == 10: stop()
     
     
