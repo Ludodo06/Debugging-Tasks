1c635c7e69ba5a344b5fc16561234b8bf71115fa
pv33apm <pv33apm@M180427>
2024-04-23 11:30:44 +0200

    autosave

--- Exercise 4 - The transporter/Bug_TheTransporter.py
+++ Exercise 4 - The transporter/Bug_TheTransporter.py
@@ -15,7 +15,7 @@ def prox():
         motor_left_target = -speed 
         motor_right_target = -speed
         
-    elif prox_horizontal[3] < 2500: 
+    elif prox_horizontal[2] < 2500: 
         nf_leds_top(32,0,0) # Rouge
         motor_left_target = speed
         motor_right_target = speed
