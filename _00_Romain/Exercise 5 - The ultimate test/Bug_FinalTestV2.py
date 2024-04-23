ba544902e0fbd5dcc1a880aaa4261de48c7bb2b1
Ludo <pujolludo@gmail.com>
2024-04-23 14:42:06 +0200

    autosave

--- _00_Romain/Exercise 5 - The ultimate test/Bug_FinalTestV2.py
+++ _00_Romain/Exercise 5 - The ultimate test/Bug_FinalTestV2.py
@@ -19,7 +19,7 @@ def prox():
         motor_left_target = 200 - steer
         motor_right_target = 200 + steer
         
-        if prox_ground_delta[0] < 1000 or prox_ground_delta[1] < 1000: # left: 0, right: 1
+        if prox_ground_delta[0] < 900 or prox_ground_delta[1] < 900: # left: 0, right: 1
             mode = LINE
             
     if mode == LINE:
