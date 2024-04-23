815625de28f2c2bba2d90888e96b4b387b237927
pv33apm <pv33apm@M180434>
2024-04-23 10:58:36 +0200

    autosave

--- Exercise 1 - Around the garden/Bug1/Bug1_Garden.py
+++ Exercise 1 - Around the garden/Bug1/Bug1_Garden.py
@@ -15,7 +15,7 @@ def timer0():
     else:
         timer_period[0] = duree_tourne
         nf_leds_top(32,16,0) # Orange
-        motor_right_target = 200
+        motor_right_target = -200
         motor_left_target = 200
         
         
