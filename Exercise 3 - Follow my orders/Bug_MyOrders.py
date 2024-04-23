a48adab40b672ab922b69c4c2a028df31ae0664a
pv33apm <pv33apm@M180423>
2024-04-23 11:12:14 +0200

    autosave

--- Exercise 3 - Follow my orders/Bug_MyOrders.py
+++ Exercise 3 - Follow my orders/Bug_MyOrders.py
@@ -1,7 +1,8 @@
 @onevent
 def buttons():
     global motor_left_target
-    global motor_right_traget
+    global motor_right_target
+    
     if button_forward:
         nf_leds_top(0,32,0)
         motor_left_target = 200
