cd5d0e3998d154280fce5fc834aa965e8875ab1e
pv33apm <pv33apm@M180434>
2024-04-30 10:31:40 +0200

    autosave

--- Exercice 0 - Leçon/events.py
+++ Exercice 0 - Leçon/events.py
@@ -4,12 +4,13 @@ def buttons():                                     # Appelé quand un bouton est
     global motor_left_target, motor_right_target   # Nécessaire pour modifier des variables globales
     
     if button_forward:                             # Vérifie si le bouton avant est pressé
-        motor_left_target = 250                   # si oui, avance
+        motor_left_target = 25                   # si oui, avance
         motor_right_target = 25
     elif button_left:                            # Sinon, si c'est le bouton central
-        motor_left_target =                    # si oui, avance
+        motor_left_target = 250                   # si oui, avance
         motor_right_target = 25
-    
+    elif button_right:
+        motor_le
         
 @onevent
 def prox():                                        # Appelé périodiquement
