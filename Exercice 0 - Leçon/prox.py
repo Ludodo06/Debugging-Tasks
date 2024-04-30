59dc572e78a055205a364efeb6d8303aa00e4f36
pv33apm <pv33apm@M180427>
2024-04-30 10:21:20 +0200

    autosave

--- Exercice 0 - Leçon/prox.py
+++ Exercice 0 - Leçon/prox.py
@@ -3,14 +3,14 @@ def prox():           # Appelé périodiquement pour vérifier les valeurs des c
     
     # Changez la valeur entre crochets pour afficher la valeur du capteur sélectionné
     #print(prox_horizontal[3])
-    
-    
-    
+
+
+
     # Vous pouvez faire pareil avec le capteur de sol
-     print(prox_ground_delta[6])
-    
-    
-    
+    print(prox_ground_delta[0])
+
+
+
     pass
 
 
