38a4cf148a7e7dbbed9634484e598159d61172bb
pv33apm <pv33apm@M180430>
2024-04-30 11:18:33 +0200

    autosave

--- Exercise 3 - Follow my orders/Bug_MyOrders.py
+++ Exercise 3 - Follow my orders/Bug_MyOrders.py
@@ -20,8 +20,8 @@ def buttons():
         
     if button_forward:
         nf_leds_top(32,0,0)
-        motor_left_target = -255
-        motor_right_target = -255
+        motor_left_target = 255
+        motor_right_target = 255
         
     if button_center:
         nf_leds_top(0,0,0)
