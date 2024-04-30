94fdbf1e281048dd12919a84b505a15b3de6e519
pv33apm <pv33apm@M180423>
2024-04-30 10:38:53 +0200

    autosave

--- Exercice 0 - Leçon/events.py
+++ Exercice 0 - Leçon/events.py
@@ -8,7 +8,7 @@ def buttons():                                     # Appelé quand un bouton est
         motor_right_target = 250
     elif button_left:                            # Sinon, si c'est le bouton central
         motor_left_target = 200                   # si oui, avance
-        motor_right_target = 100                                    # Ligne à modifier (ajouter un comportement)
+        motor_right_target = 60                                    # Ligne à modifier (ajouter un comportement)
         
         
 @onevent
