3078835709a9b3ad5bbaf520113d0da1fd55c22d
Ludo <pujolludo@gmail.com>
2024-05-01 14:51:31 +0200

    autosave

--- _01_ClaraWetzel/Exercise 5 - The ultimate test/Bug_FinalTestV2.py
+++ _01_ClaraWetzel/Exercise 5 - The ultimate test/Bug_FinalTestV2.py
@@ -14,7 +14,7 @@ def prox():
         nf_leds_top(32,0,0) # Red
         steer = (prox_horizontal[0] - prox_horizontal[4])//20        # left: 0, right: 4
         
-        motor_left_target = 200 - steer
+        motor_left_target = 200 -steer
         motor_right_target = 200 - steer
         
         if prox_ground_delta[0] < 1000 or prox_ground_delta[1] < 1000: # left: 0, right: 1
