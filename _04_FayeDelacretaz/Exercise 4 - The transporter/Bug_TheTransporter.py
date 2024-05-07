6c771239541328c820d4d149662502fb2dbf7c88
Ludo <pujolludo@gmail.com>
2024-05-07 11:28:48 +0200

    autosave

--- _04_FayeDelacretaz/Exercise 4 - The transporter/Bug_TheTransporter.py
+++ _04_FayeDelacretaz/Exercise 4 - The transporter/Bug_TheTransporter.py
@@ -10,12 +10,12 @@ def prox():
         
     nf_leds_top(32,16,0) # Orange
 
-    if prox_horizontal[front] < 2500:
+    if prox_horizontal[front] > 2500:
         nf_leds_top(0,32,0) # Green
         motor_left_target = -speed 
         motor_right_target = -251
         
-    elif prox_horizontal[7] < 2500: 
+    elif prox_horizontal[back] > 2500: 
         nf_leds_top(32,0,0) # Red
         motor_left_target = 251
         motor_right_target = speed
