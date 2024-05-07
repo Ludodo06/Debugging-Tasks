f120a0236ba6194419ba36b1f36fe81bcc991946
Ludo <pujolludo@gmail.com>
2024-05-07 10:53:04 +0200

    autosave

--- _04_FayeDelacretaz/Exercice 0 - Leçon/prox.py
+++ _04_FayeDelacretaz/Exercice 0 - Leçon/prox.py
@@ -4,7 +4,7 @@ def prox():
     # Use print here to show the proximity sensors' values
     # print(...)
     
-    
+    print(prox_horizontal
     
     # You can also print the ground sensors here
     # print(...)
