d7577980a840ba9195db16fe84580154a0c4db7e
Ludo <pujolludo@gmail.com>
2024-05-08 15:23:19 +0200

    autosave

--- _06_LisaWenjie/Exercise 1 - Around the garden/Bug1/Bug1_Garden.py
+++ _06_LisaWenjie/Exercise 1 - Around the garden/Bug1/Bug1_Garden.py
@@ -8,12 +8,16 @@ going_forward = False
 def timer0():
     global leds_top, going_forward, motor_left_target, motor_right_target
     going_forward = not going_forward
-    if going_forward:
+   
+   
+   if going_forward:
         timer_period[0] = forward_time
         nf_leds_top(0,32,0) # Green
         motor_right_target = 200
         motor_left_target = 200
-    else:
+   
+   
+   else:
         timer_period[0] = turn_time
         nf_leds_top(32,16,0) # Orange
         motor_right_target = 200
