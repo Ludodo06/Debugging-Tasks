6c30dbbd922796c87da1a704beddf5b85958058f
Ludo <pujolludo@gmail.com>
2024-05-08 15:29:39 +0200

    autosave

--- _06_LisaWenjie/Exercise 1 - Around the garden/Bug1/Bug1_Garden.py
+++ _06_LisaWenjie/Exercise 1 - Around the garden/Bug1/Bug1_Garden.py
@@ -21,8 +21,8 @@ def timer0():
    else:
         timer_period[0] = turn_time
         nf_leds_top(32,16,0) # Orange
-        motor_right_target = 100
-        motor_left_target = -100
+        motor_right_target = 200
+        motor_left_target = -200
         
         
         
