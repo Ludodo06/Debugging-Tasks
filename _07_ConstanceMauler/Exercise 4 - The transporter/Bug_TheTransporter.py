23116be6779bbf155cc58a663d75d481eca09f01
Ludo <pujolludo@gmail.com>
2024-05-09 11:31:34 +0200

    autosave

--- _07_ConstanceMauler/Exercise 4 - The transporter/Bug_TheTransporter.py
+++ _07_ConstanceMauler/Exercise 4 - The transporter/Bug_TheTransporter.py
@@ -18,7 +18,7 @@ def prox():
     elif prox_horizontal[6] > 2500: 
         nf_leds_top(32,0,0) # Red
         motor_left_target = 251
-        motor_right_target = speed
+        motor_right_target = 251
         
 
 
