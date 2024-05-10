94a34b6dedbf072f81a7eb7d0c1720b7fcaabf5d
Ludo <pujolludo@gmail.com>
2024-05-10 11:20:49 +0200

    autosave

--- _09_JulesCristmann/Exercise 3 - Follow my orders/Bug_MyOrders.py
+++ _09_JulesCristmann/Exercise 3 - Follow my orders/Bug_MyOrders.py
@@ -3,7 +3,7 @@ def buttons():
     global motor_left_target
     
     if button_forward:
-        nf_leds_top(0,32,0)
+        nf_leds_top(0,32,0) 
         motor_left_target = 255
         motor_right_target = 255
         
@@ -17,7 +17,7 @@ def buttons():
         motor_left_target = 100
         motor_right_target = -100
         
-    if button_forward:
+    if button_backward:
         nf_leds_top(32,0,0)
         motor_left_target = -255
         motor_right_target = -255
