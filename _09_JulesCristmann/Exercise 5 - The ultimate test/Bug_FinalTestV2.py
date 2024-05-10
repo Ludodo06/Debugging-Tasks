935477f0255f58fa4678789808a1821b77486460
Ludo <pujolludo@gmail.com>
2024-05-10 11:40:47 +0200

    autosave

--- _09_JulesCristmann/Exercise 5 - The ultimate test/Bug_FinalTestV2.py
+++ _09_JulesCristmann/Exercise 5 - The ultimate test/Bug_FinalTestV2.py
@@ -12,7 +12,7 @@ def prox():
     
     if mode == WALLS:
         nf_leds_top(32,0,0) # Red
-        
+        if prox_horizontal[0] or prox_horizontal[4] > 
         steer = (prox_horizontal[0] - prox_horizontal[4])//20        # left: 0, right: 4
         
         motor_left_target = 200 + steer
